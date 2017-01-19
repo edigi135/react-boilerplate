baf3dc863038936a704ca355702c8b06081c6c4a
Evan DiGiambattista <edigiam@me.com>
2017-01-18 22:39:42 -0500

    Remove pixi, add scorejs

--- public/bundle.js
+++ public/bundle.js
@@ -31455,23 +31455,6 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	// import pixiPianoRoll from 'pixi-piano-roll';
-	
-	// const pianoRoll = pixiPianoRoll({
-	//     width: 900,
-	//     height: 400,
-	//     noteColor: 0xdb000f,
-	//     gridLineColor: 0x333333,
-	//     backgroundColor: 0x1a0002,
-	//     bpm: 140,
-	//     antialias: true,
-	//     zoom: 4,
-	//     resolution: 2,
-	//     time: '0:0:0',
-	//     renderer: 'WebGLRenderer',
-	//     noteFormat: 'String'
-	// });
-	
 	exports.default = function () {
 		return _react2.default.createElement(
 			'div',
